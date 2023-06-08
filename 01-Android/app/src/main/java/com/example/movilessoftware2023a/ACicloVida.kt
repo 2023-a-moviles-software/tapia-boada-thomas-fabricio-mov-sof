3cefb144f8645b6777ece86b47c7ae27af9b30fd
Thomas Tapia <thomas.tapia@epn.edu.ec>
2023-06-08 08:41:54 -0500

    ACicloVida

---
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/ACicloVida.kt
@@ -0,0 +1,20 @@
+package com.example.movilessoftware2023a
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ACicloVida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aciclo_vida)
+    }
+
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
+        //this.startActivity()
+    }
+}
