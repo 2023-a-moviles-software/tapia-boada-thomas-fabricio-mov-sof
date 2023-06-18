d996ac5b44dea22483199302fa3e121d7ff755fe
Thomas <89487644+Ttdm34@users.noreply.github.com>
2023-06-18 17:44:04 -0500

    Arreglar json

--- X1_Deber/.idea/sonarlint/issuestore/index.pb
+++ X1_Deber/.idea/sonarlint/issuestore/index.pb
@@ -1,13 +1,9 @@
 
 e
 5TiendaFrutas/gradle/wrapper/gradle-wrapper.properties,9\5\9575a1786e4c29ad8614058801785af1be27a6b9
-M
-TiendaFrutas/build.gradle.kts,0\0\009bf8bbc63a749b60cc260aa7cd63084e5a9094
 P
  TiendaFrutas/settings.gradle.kts,a\8\a82b91116b33f66a5eef382b3a7c33cb06936dae
 N
 TiendaFrutas/gradle.properties,5\d\5dcd85c7ec4fc9df10a6d596daeeda491fe55afc
 V
 &TiendaFrutas/src/main/kotlin/Clases.kt,0\1\015d78487124b7c741cf6e5660029f0669e4d80f
-T
-$TiendaFrutas/src/main/kotlin/Menu.kt,a\9\a91b06c1379cf23c16401b46dc0c4d77cb40edb5
--- X1_Deber/TiendaFrutas/src/main/kotlin/Menu.kt
+++ X1_Deber/TiendaFrutas/src/main/kotlin/Menu.kt
@@ -21,7 +21,6 @@ fun main(args: Array<String>) {
     try{
         tienda = Json.decodeFromString(File("resources/tiendita.json").readText())
         nombre = tienda?.nombreTienda
-
     }
     catch (e : FileNotFoundException){
         println("Ingrese el nombre de la tienda: ")
@@ -139,24 +138,5 @@ fun main(args: Array<String>) {
             }
         }
     }
-
-
-
-//    val tienda1 = Tienda("Fruteria Thomas","Quito",1234567890,983359387,"Thomas Tapia")
-//    val fruta1 = Fruta("Pera",0.25,10,"Manzana")
-//    val fruta2 = Fruta("Manzana",0.15,5,"Manzana")
-//    tienda1.añadirFruta(fruta1)
-//    tienda1.añadirFruta(fruta2)
-//    tienda1.frutas.forEach{println(it.nombreFruta +" " + it.disponibilidad)}
-//    tienda1.comprarFruta(1,10)
-//    tienda1.comprarFruta(2,2)
-//    tienda1.finalizarCompra()
-//    tienda1.añadirCantidadFruta(1,1)
-//    tienda1.comprarFruta(1,3)
-//    tienda1.comprarFruta(2,1)
-//    tienda1.finalizarCompra()
-
-//    tienda1.eliminarFruta(2)
-//    tienda1.frutas.forEach{println(it.nombreFruta +" " + it.disponibilidad)}
 
 }
