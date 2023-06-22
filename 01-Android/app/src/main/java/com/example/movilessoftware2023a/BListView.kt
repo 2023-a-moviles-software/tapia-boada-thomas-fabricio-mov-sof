8ba02cf0aad533b2f5760a2cb1460958e1e4475d
Thomas Tapia <thomas.tapia@epn.edu.ec>
2023-06-22 09:21:34 -0500

    List View

--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/BListView.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/BListView.kt
@@ -2,10 +2,75 @@ package com.example.movilessoftware2023a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 
 class BListView : AppCompatActivity() {
+    val arreglo = BBaseDatosMemoria.arregloBEntrenador
+    var idItemSeleccionado = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blist_view)
+        // adaptador (Iterables)
+        val listView = findViewById<ListView>(R.id.lv_list_view)
+        val adaptador = ArrayAdapter(
+            this, // contexto
+            android.R.layout.simple_list_item_1, //layout.xml que se va a usar
+            arreglo
+        )
+        listView.adapter = adaptador
+        adaptador.notifyDataSetChanged()
+        val botonaAnadirListView = findViewById<Button>(R.id.btn_anadir_list_view)
+        botonaAnadirListView.setOnClickListener {
+            anadirEntrenador(adaptador)
+        }
+        registerForContextMenu(listView)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.mi_editar -> {
+                "Hacer algo con ${idItemSeleccionado}"
+                return true
+            }
+
+            R.id.mi_eliminar -> {
+                "Hacer algo con ${idItemSeleccionado}"
+                return false
+            }
+
+            else -> super.onContextItemSelected(item)
+
+        }
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        //Llenar las opciones del menu
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        //obtener el id del ArrayList seleccionado
+        val info = menuInfo as AdapterView.AdapterContextMenuInfo
+        val id = info.position
+        idItemSeleccionado = id
+    }
+
+    fun anadirEntrenador(
+        adaptador: ArrayAdapter<BEntrenador>
+    ){
+        arreglo.add(
+            BEntrenador(4, "Dorys", "d@d.com")
+        )
+        adaptador.notifyDataSetChanged()
     }
 }
+
