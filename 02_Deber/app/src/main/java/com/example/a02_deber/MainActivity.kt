3dd87c787d47874ff59a7a65fc5028dabdab97a8
Thomas <89487644+Ttdm34@users.noreply.github.com>
2023-07-19 21:52:16 -0500

    Diseño de UI

--- 02_Deber/app/src/main/java/com/example/a02_deber/MainActivity.kt
+++ 02_Deber/app/src/main/java/com/example/a02_deber/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.a02_deber
 
+import android.app.UiModeManager.MODE_NIGHT_NO
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = this.resources.getColor(R.color.dark_blue)
     }
 }
