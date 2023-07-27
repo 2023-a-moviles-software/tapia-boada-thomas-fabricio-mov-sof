3a174bad2df5cd1f871e98baa46cd0e52cbae709
Thomas <89487644+Ttdm34@users.noreply.github.com>
2023-07-27 09:18:18 -0500

    Google Maps

--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/GGoogleMaps.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/GGoogleMaps.kt
@@ -3,6 +3,8 @@ package com.example.movilessoftware2023a
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -11,6 +13,8 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.PolylineOptions
 
 class GGoogleMaps : AppCompatActivity() {
     private lateinit var mapa : GoogleMap
@@ -19,6 +23,19 @@ class GGoogleMaps : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ggoogle_maps)
+        solicitarPermisos()
+        iniciarLogicaMapa()
+        val boton = findViewById<Button>(R.id.btn_ir_carolina)
+        boton.setOnClickListener {
+            irCarolina()
+        }
+    }
+
+    fun irCarolina(){
+        val carolina = LatLng(-0.1825684318486696,
+        -78.48447277600916)
+        val zoom = 17f
+        moverCamaraConZoom(carolina,zoom)
     }
 
     fun marcadorQuicentro(){
@@ -29,6 +46,46 @@ class GGoogleMaps : AppCompatActivity() {
         markQuicentro.tag = titulo
         moverCamaraConZoom(quicentro)
     }
+
+    fun anadirPolilinea(){
+        with (mapa){
+            val poliLineaUno = mapa
+                .addPolyline(
+                    PolylineOptions()
+                        .clickable(true)
+                        .add(
+                            LatLng(-0.1759187040647396,
+                            -78.48506472421384),
+                        LatLng(-0.17632468492901104,
+                        -78.48265589308046),
+                            LatLng(-0.17746143130181483,
+                            -78.4770533307815)
+                        )
+                )
+            poliLineaUno.tag = "Linea-1" // <- ID
+        }
+    }
+
+    fun anadirPoligono(){
+        with(mapa){
+            val poligonoUno = mapa
+                .addPolygon(
+                    PolygonOptions()
+                        .clickable(true)
+                        .add(
+                            LatLng(-0.1771546902239471,
+                            -78.48344981495214),
+                            LatLng(-0.17968981486125768,
+                            -78.48269198043828),
+                            LatLng(-0.17710958124147777,
+                            -78.48142892291516)
+                        )
+                )
+            poligonoUno.fillColor = -0xc771c4
+            poligonoUno.tag = "poligono-2" // <- ID
+        }
+    }
+
 
     fun solicitarPermisos(){
         val contexto = this.applicationContext
@@ -52,28 +109,60 @@ class GGoogleMaps : AppCompatActivity() {
     }
 
     fun iniciarLogicaMapa(){
-        val fragmentoMapa = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        val fragmentoMapa = supportFragmentManager
+            .findFragmentById(R.id.map_ggogle) as SupportMapFragment
         fragmentoMapa.getMapAsync { googleMap ->
-            with(googleMap){ //googleMap != NULL
+            with(googleMap){ // googleMap != null
                 mapa = googleMap
                 establecerConfiguracionMapa()
+                marcadorQuicentro()
+                anadirPoligono()
+                anadirPolilinea()
+                escucharListeners()
             }
         }
     }
 
     fun establecerConfiguracionMapa(){
         val contexto = this.applicationContext
-        with(mapa){
-            val permisosFineLocation = ContextCompat.checkSelfPermission(
-                contexto,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            )
+        with(mapa) {
+            val permisosFineLocation = ContextCompat
+                .checkSelfPermission(
+                    contexto,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
             val tienePermisos = permisosFineLocation == PackageManager.PERMISSION_GRANTED
-            if (tienePermisos){
-                mapa.isMyLocationEnabled = true //tenemos permisos
+            if (tienePermisos) {
+                mapa.isMyLocationEnabled = true //  tenemos permisos
                 uiSettings.isMyLocationButtonEnabled = true
             }
             uiSettings.isZoomControlsEnabled = true
+        }
+    }
+
+
+    fun escucharListeners(){
+        mapa.setOnPolygonClickListener {
+            Log.i("mapa", "setOnPolygonCLickListener ${it}")
+            it.tag //ID
+        }
+        mapa.setOnPolylineClickListener {
+            Log.i("mapa", "setOnPolylineClickListener ${it}")
+            it.tag //ID
+        }
+        mapa.setOnMarkerClickListener {
+            Log.i("mapa", "setOnMarkerClickListener ${it}")
+            it.tag //ID
+            return@setOnMarkerClickListener true
+        }
+        mapa.setOnCameraMoveListener {
+            Log.i("mapa", "setOnCameraMoveListener ")
+        }
+        mapa.setOnCameraMoveStartedListener {
+            Log.i("mapa", "setOnCameraMoveStartedListener ${it}")
+        }
+        mapa.setOnCameraIdleListener {
+            Log.i("mapa", "setOnCameraIdleListener")
         }
     }
 
--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/GoogleMapsInicio.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/GoogleMapsInicio.kt
@@ -24,7 +24,7 @@ class GoogleMapsInicio : AppCompatActivity(), OnMapReadyCallback {
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+            .findFragmentById(R.id.map_ggogle) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
