84f71f7f0d815e113b8abc78d4807119dad879e9
ThomasTapia <thomas.tapia@epn.edu.ec>
2023-08-03 10:11:26 -0500

    Mejora Autenticación con Google

--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/HFirebaseUIAuth.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/HFirebaseUIAuth.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.IdpResponse
@@ -29,6 +30,8 @@ class HFirebaseUIAuth : AppCompatActivity() {
     ){
         val btnLogin : Button = findViewById<Button>(R.id.btn_login_firebase)
         val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
+        val tvBienvenido = findViewById<TextView>(R.id.tv_bienvenido)
+        tvBienvenido.text = FirebaseAuth.getInstance().currentUser?.displayName
         btnLogin.visibility = View.INVISIBLE
         btnLogout.visibility = View.VISIBLE
         if(res.isNewUser == true){
@@ -58,12 +61,23 @@ class HFirebaseUIAuth : AppCompatActivity() {
         }
         val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
         btnLogout.setOnClickListener { seDeslogeo() }
+        val usuario = FirebaseAuth.getInstance().currentUser
+        if (usuario != null){
+            val tvBienvenido = findViewById<TextView>(R.id.tv_bienvenido)
+            val btnLogin = findViewById<Button>(R.id.btn_login_firebase)
+            val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
+            btnLogout.visibility = View.VISIBLE
+            btnLogin.visibility = View.INVISIBLE
+            tvBienvenido.text = usuario.displayName
+        }
 
     }
 
     fun seDeslogeo(){
         val btnLogin = findViewById<Button>(R.id.btn_login_firebase)
         val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
+        val tvBienvenido = findViewById<TextView>(R.id.tv_bienvenido)
+        tvBienvenido.text = "Bienvenido"
         btnLogout.visibility = View.INVISIBLE
         btnLogin.visibility = View.VISIBLE
         FirebaseAuth.getInstance().signOut()
