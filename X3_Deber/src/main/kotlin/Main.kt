2c910afa9f59cde4a1fcf032eb739364cadd84fe
ThomasTapia <thomas.tapia@epn.edu.ec>
2023-08-10 15:07:27 -0500

    Funciones para Tienda

--- X3_Deber/src/main/kotlin/Main.kt
+++ X3_Deber/src/main/kotlin/Main.kt
@@ -3,8 +3,12 @@ fun main(args: Array<String>) {
 
     val baseDatosSQLite = SQLite()
     baseDatosSQLite.createTable()
-    println("Hello World!")
-
+//    baseDatosSQLite.crearTienda(3,"TiendamaniaRemake","1724443294001",983359387,"Thomas Tapia")
+    println(baseDatosSQLite.consultarTiendas())
+    baseDatosSQLite.eliminarTiendaPorId(1)
+    println(baseDatosSQLite.consultarTiendas())
+    baseDatosSQLite.actualizarTienda(2, "PepeTienda", "1712581329001",null, null)
+    println(baseDatosSQLite.consultarTiendas())
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
--- X3_Deber/src/main/kotlin/SQLite.kt
+++ X3_Deber/src/main/kotlin/SQLite.kt
@@ -7,14 +7,14 @@ class SQLite {
     private val connection : Connection = DriverManager.getConnection("jdbc:sqlite:X3_Deber.db")
     private val statement : Statement = connection.createStatement()
 
-    fun createTable(){
+    fun createTable() {
         val checkTableTienda = "SELECT name FROM sqlite_master WHERE type='table' AND name='TIENDA';"
         val checkTableFruta = "SELECT name FROM sqlite_master WHERE type='table' AND name='FRUTA'"
 
         val tiendaTableExist = statement.executeQuery(checkTableTienda).next()
         val frutaTableExist = statement.executeQuery(checkTableFruta).next()
 
-        if (!tiendaTableExist){
+        if (!tiendaTableExist) {
             val createTiendaTable = """
                 CREATE TABLE TIENDA (
                 id_tienda           INTEGER   PRIMARY KEY NOT NULL,
@@ -27,7 +27,7 @@ class SQLite {
             statement.executeQuery(createTiendaTable)
         }
 
-        if (!frutaTableExist){
+        if (!frutaTableExist) {
             val createFrutaTable = """
                 CREATE TABLE FRUTA (
                 id_fruta        INTEGER    PRIMARY KEY AUTOINCREMENT,
@@ -40,11 +40,77 @@ class SQLite {
             """.trimIndent()
             statement.executeQuery(createFrutaTable)
         }
-
+    }
         fun close(){
             statement.close()
             connection.close()
         }
 
-    }
+        //Funciones para tiendas
+        fun crearTienda(idTienda : Int, nombreTienda:String, ruc:String, telefono:Int, nombrePropietario:String):Boolean {
+            val query = """
+                INSERT INTO TIENDA (id_tienda, nombre_tienda, ruc, telefono, nombre_propietario)
+                VALUES ('$idTienda','$nombreTienda','$ruc','$telefono','$nombrePropietario')
+            """.trimIndent()
+            try {
+                statement.executeQuery(query)
+                return true
+            } catch (e: Exception){
+                return false
+            }
+        }
+
+        fun consultarTiendas() : ArrayList<Tienda>{
+            var tiendas = ArrayList<Tienda>()
+            val query = """
+                SELECT * FROM TIENDA
+            """.trimIndent()
+            try {
+                val result = statement.executeQuery(query)
+                while (result.next()){
+                    tiendas.add(Tienda(result.getInt(1),result.getString(2),
+                        result.getString(3), result.getInt(4),result.getString(5)))
+                }
+                return tiendas
+                } catch (e: Exception){
+                return  tiendas
+                }
+        }
+
+        fun eliminarTiendaPorId(idTienda: Int):Boolean {
+            val query = """
+                DELETE FROM TIENDA
+                WHERE id_tienda = '$idTienda'
+            """.trimIndent()
+            try {
+                statement.executeQuery(query)
+                return true
+            } catch (e : Exception){
+                return false
+            }
+        }
+
+        fun actualizarTienda(idTienda: Int, nuevoNombreTienda: String?, nuevoRuc: String?, nuevoTelefono: Int?, nuevoNombrePropietario: String?): Boolean {
+            val query = """
+                UPDATE TIENDA
+                SET nombre_tienda = ?, ruc = ?, telefono = ?, nombre_propietario = ?
+                WHERE id_tienda = ?
+            """.trimIndent()
+            try {
+                val preparedStatement = connection.prepareStatement(query)
+                preparedStatement.setString(1,nuevoNombreTienda)
+                preparedStatement.setString(2,nuevoRuc)
+                preparedStatement.setInt(3,nuevoTelefono ?: 0)
+                preparedStatement.setString(4,nuevoNombrePropietario)
+                preparedStatement.setInt(5, idTienda)
+
+                preparedStatement.executeUpdate()
+                preparedStatement.close()
+                return true
+            } catch (e: Exception){
+                return false
+            }
+        }
+
+
 }
